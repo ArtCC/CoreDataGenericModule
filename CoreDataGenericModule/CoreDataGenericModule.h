1c1a560c04e9d15eaaf090fab8ca765e5a015f13
Arturo C. Calvo <artccmail@gmail.com>
2016-10-27 11:09:44 +0200

    Initial Commit

---
+++ CoreDataGenericModule/CoreDataGenericModule.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataGenericModule.h
+//  CoreDataGenericModule
+//
+//  Created by Arturo Carretero Calvo on 27/10/16.
+//  Copyright © 2016 Arturo Carretero Calvo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataGenericModule.
+FOUNDATION_EXPORT double CoreDataGenericModuleVersionNumber;
+
+//! Project version string for CoreDataGenericModule.
+FOUNDATION_EXPORT const unsigned char CoreDataGenericModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataGenericModule/PublicHeader.h>
+
+
